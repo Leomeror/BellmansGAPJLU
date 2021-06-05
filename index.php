4d7e753eef67143ee668260981c3d0c9ead302f6
Leomeror <52535340+Leomeror@users.noreply.github.com>
2021-06-05 19:51:34 +0200

    Updated index.php

--- index.php
+++ index.php
@@ -1,105 +1,169 @@
 Es kann momentan nur elmamun.gap als Grammatik benutzt werden.
 <br><br>
-
-<form action="index.php" method="get">
-Gib die Formel ein, deren Klammerstellung du mit Elmamun prüfen wilst <br> <input type="text" name="input" value='<?php echo isset($_GET['input']) ? $_GET['input'] : '' ?>'>
-<br><br>
-Wähle die Instanz mit den Algebren die du verwenden möchtest.
-<select name="algebra">
-<option <?php if ($_GET['algebra'] == 'pp') { echo " selected "; };?> value="pp" >alg_pretty</option>
-<option <?php if ($_GET['algebra'] == 'enum') { echo " selected "; };?>value="enum">alg_enum</option>
-<option <?php if ($_GET['algebra'] == 'sellerpp') { echo " selected "; };?>value="sellerpp">alg_seller * alg_pretty</option>
-<option <?php if ($_GET['algebra'] == 'buyerpp') { echo " selected "; };?>value="buyerpp">alg_buyer * alg_pretty</option>
-<option <?php if ($_GET['algebra'] == 'ppbuyer') { echo " selected "; };?>value="ppbuyer">alg_pretty * alg_buyer</option>
-<option <?php if ($_GET['algebra'] == 'timepp') { echo " selected "; };?>value="timepp">alg_time * alg_pretty</option>
-</select>
-<br><br>
-<!--
-<input type="submit">
-</form>
--->
-
-<!--
-<p>Algebra: 
-<form action="" method="post">
-<SELECT NAME="algebra1">
-<OPTION value="alg_pretty" SELECTED>alg_pretty</OPTION><OPTION value="alg_enum">alg_enum</OPTION><OPTION value="alg_seller">alg_seller</OPTION><OPTION value="alg_buyer">alg_buyer</OPTION><OPTION value="alg_time">alg_time</OPTION>
-</SELECT>
-<input type="submit" name="submit" value="Choose.">
-</form>
-und das ist
-<?php 
-echo bla;
-$algebra1 = $_POST['algebra1']; 
-echo $algebra1;
-?>
-***
-<form action="" method=post">
-<SELECT NAME="algebra2">
-<OPTION SELECTED 
 <?php
-if ($algebra1 == "alg_pretty") {
-    echo 'value="-">"-"';
-    echo '</OPTION><OPTION value= "alg_buyer">alg_buyer';
-} elseif ($algebra1 == "alg_enum") {
-    echo 'value="-">"-"';
-} else {
-    echo 'value="alg_pretty">alg_pretty';
-}
+    session_start();
+    if(!isset($_SESSION['Grammatikdatei'])){
+    $_SESSION['Grammatikdatei']=$_GET['Grammatikdatei'];}
+    
+    if(isset($_GET['Grammatikdatei'])){
+    $_SESSION['Grammatikdatei'] = $_GET['Grammatikdatei'];}
 ?>
-</OPTION></SELECT>
-<input type ="submit" name="submit" value="Choose">
+<form id="form1" action="index.php" method="get">
+Wähle zuerst aus welche Grammatik du verwenden willst.<br>
+Durch drücken auf "Parsen" werden dir weiter unten alle möglichen Algebren zur Auswahl angezeigt.<br>
+Grammar:
+<select name="Grammatikdatei" >
+    <option <?php if ($_SESSION['Grammatikdatei'] == '-') { echo " selected "; };?> value="-">-</option>
+    <option <?php if ($_SESSION['Grammatikdatei'] == 'elmamun.gap') { echo " selected "; };?> value="elmamun.gap">Elmamun</option>
+</select>
+<!--
+    Muss Seite davon abhalten, die shell_exec Befehle auszuführen, bis der untere Submit Button gedrückt wurde. DONE
+    Der obere Submit-Button soll nur das Parsen aktivieren.
+    Mi-2.6.2021-18:00-19:15 : 1 Stunde 15 min
+    Do. 3.6.2021 14:42-17:42 : 3 Stunden
+    Sa 5.6.2021 Docker-Videos 11:30-13:10 :1 Std. 40 min 
+    Sa 5.6.2021 14:00-14:20 : 20 min
+    Sa 5.6.2021 18:27 :  min
+    
+-->
+<input type="submit" name="parse" value="Parsen">
+<br>
 </form>
-&nbsp;&nbsp;&nbsp;
-<INPUT TYPE=SUBMIT NAME="CommandButton1" VALUE="Go!">
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-</p>
--->
+<?php
+    
+    $Grammatikdatei = $_SESSION['Grammatikdatei'];
+    
+    if(!isset($algebras))
+    {
+        $algebras = [];
+    }
+    
+    if(array_key_exists('parse',$_GET)){
+        if($Grammatikdatei=="-"){
+            echo '<span style="color:red">';
+            echo "Wähle bitte eine Grammatik aus!";
+            echo "</span>";
+        }
+        else{
+            $gap_file= file_get_contents($Grammatikdatei);
+            $rows = explode("\n", $gap_file);
+            foreach($rows as $row){
+                $row_content=explode(" ", $row);
+                if($row_content[0]=="algebra"){
+                    $algebras[] = $row_content[1];
+                }
+            }
+        }
+        $_SESSION['algebras']=$algebras;
+    }
+?>
 
-<!-- Instanzen in elmamun.gap:
-        instance pp = gra_elmamun(alg_pretty);
-        instance enum = gra_elmamun(alg_enum);
-        instance sellerpp = gra_elmamun(alg_seller * alg_pretty);
-        instance buyerpp = gra_elmamun(alg_buyer * alg_pretty);
-        instance ppbuyer = gra_elmamun(alg_pretty * alg_buyer);
-        instance timepp = gra_elmamun(alg_time * alg_pretty);
--->
+<form id="form2" action="" method="get">
+Wähle die Algebren die du verwenden möchtest.
+<br>Algebra:
 
-
-Wähle aus welche Grammatik du verwenden willst.
-<select name="Grammatikdatei">
-    <option value="elmamun.gap">Elmamun</option>
+<select name="algebra1">
+<?php
+    foreach($_SESSION['algebras'] as $algebra){
+        echo "<option ";
+        if ($_GET['algebra1'] == $algebra) { echo " selected "; };
+        echo 'value="'.$algebra.'">'.$algebra.'</option>';
+    }
+?>
 </select>
 
-<input type="submit">
+***
+
+<select name="algebra2">
+<option <?php if ($_GET['algebra2'] == '-') { echo " selected "; };?> value="-">-</option>
+<?php
+    foreach($_SESSION['algebras'] as $algebra){
+        echo "<option ";
+        if ($_GET['algebra2'] == $algebra) { echo " selected "; };
+        echo 'value="'.$algebra.'">'.$algebra.'</option>';
+    }
+?>
+</select>
+<br>
+
+Und gib die Formel ein, deren Klammerstellung du mit Elmamun prüfen willst.<br>
+Input:<input type="text" name="input" placeholder="2+1*3" value='<?php echo isset($_GET['input']) ? $_GET['input'] : '' ?>'>
+(z.B.: 2+1*3)
+<br>
+
+&nbsp &nbsp
+
+<input type="submit" name="submit">
+
+</form>
+<?php
+    if(isset($_GET["submit"])){
+        if(($_GET['input']!="")){
+            echo '<span style="color:green">';
+            echo "Die Einträge wurden an die Website übermittelt.";
+            echo "</span>";
+        }
+        else{
+            echo '<span style="color:red">';
+            echo "Gib einen Input ein!";
+            echo "</span>";
+        }
+    }
+?>
+<br><br>
+
+<form method="post">
+Mit Klicken auf "Submit" werden deine Einträge in der Website gespeichert.
+<br>
+Du kannst jetzt auf "RUN" klicken um alles mit dem gapc Compiler auszurechnen.
+<br>
+    <input type="submit" name="go" id="go" value="RUN" /><br/>
 </form>
 
 <?php
 $input = $_GET['input'];
+$algebra1 = $_GET['algebra1'];
+$algebra2 = $_GET['algebra2'];
+
+function useGapcSingleAlgebra($algebra1, $Grammatikdatei, $input)
+{
 echo "<pre>"; 
-/*   alter Code
-$algebra = $_GET['algebra']; 
-if ($algebra == "alg_pretty") {
-    $instance = pp;
-}   elseif ($algebra == "alg_enum") {
-    $instance = enum;
-}   elseif ($algebra == "alg_seller * alg_pretty") {
-    $instance = sellerpp;
-}   elseif ($algebra == "alg_buyer * alg_pretty") {
-    $instance = buyerpp;
-}   elseif ($algebra == "alg_pretty * alg_buyer") {
-    $instance = ppbuyer;
-}   elseif ($algebra == "alg_time * alg_pretty") {
-    $instance = timepp;
+if (!file_exists($algebra1."_gapc.cc")){
+    echo shell_exec('gapc -p '.$algebra1.' -o '.$algebra1.'_gapc.cc '.$Grammatikdatei.' 2>&1');
 }
-*/
-$instance = $_GET['algebra'];
-$Grammatikdatei = $_GET['Grammatikdatei'];
-if (!file_exists($instance."gapc.cc")){
-    shell_exec("gapc -i ".$instance." -o ".$instance."gapc.cc ".$Grammatikdatei);
-}
-shell_exec("make -f ".$instance."gapc.mf");
-$result = shell_exec("./".$instance."gapc ". $input);
+shell_exec("make -f ".$algebra1."_gapc.mf"." 2>&1");
+$result = shell_exec("./".$algebra1."_gapc ".$input." 2>&1");
 echo $result;
 echo "</pre>";
+}
+
+function useGapcAlgebraProduct($algebra1, $algebra2, $Grammatikdatei, $input)
+{
+echo "<pre>"; 
+$operator= '*';
+
+if (!file_exists($algebra1."_".$algebra2."_gapc.cc")){
+    echo shell_exec('gapc -p '.$algebra1.$operator.$algebra2.' -o '.$algebra1.'_'.$algebra2.'_gapc.cc '.$Grammatikdatei.' 2>&1');
+}
+shell_exec("make -f ".$algebra1."_".$algebra2."_gapc.mf"." 2>&1");
+$result = shell_exec("./".$algebra1."_".$algebra2."_gapc ".$input." 2>&1");
+echo $result;
+echo "</pre>";
+}
+
+
+if(array_key_exists('go',$_POST)){
+    //Ist unnötig, weil oben schon abgefragt:
+    if($Grammatikdatei =="-"){
+        $Grammatikdatei="";
+    }
+    //Wenn nur eine Algebra angegeben, benutze -p mit nur Algebra1(=useGapcSingleAlgebra)
+    if($algebra2 == "-"){
+        useGapcSingleAlgebra($algebra1, $Grammatikdatei, $input);
+    }
+    else{
+        useGapcAlgebraProduct($algebra1, $algebra2, $Grammatikdatei, $input);
+    }
+}
+
 ?>
